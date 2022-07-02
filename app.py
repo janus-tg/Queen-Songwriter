ce67a06b25975863effc2d0733a277a377862319
janus-tg <65816394+janus-tg@users.noreply.github.com>
2022-07-02 16:01:08 -0400

    Work on front end has reached completion. Waiting for model weights to download. Will just load model using weights and generate new song lyrics

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def formInput():
+    text = request.form['text']
+    print(text)
+    return render_template('index.html', oldInput = text, newLyrics = text + "resultwefn urwibhiwufw euiore2or oeirhe20or oehri2oebr oeirhoeb oierhoerbh oehroie2hro oeiufewo")
 
 if __name__ == "__main__":
     app.run(debug=True)
